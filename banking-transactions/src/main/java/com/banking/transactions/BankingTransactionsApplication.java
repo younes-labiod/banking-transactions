74df924f99557afc300c826718d82dc83a2422b9
younes-labiod <youneslabiod@gmail.com>
2023-04-13 00:35:02 +0400

    Skeleton preparation | Initial commit

---
+++ banking-transactions/src/main/java/com/banking/transactions/BankingTransactionsApplication.java
@@ -0,0 +1,13 @@
+package com.banking.transactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingTransactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingTransactionsApplication.class, args);
+	}
+
+}
---
+++ banking-transactions/src/test/java/com/banking/transactions/BankingTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.transactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingTransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
