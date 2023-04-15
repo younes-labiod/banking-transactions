3e23bfd81bb1cc724562213a43e5136cbb93ad38
younes-labiod <youneslabiod@gmail.com>
2023-04-15 15:35:44 +0400

    Validations API | bug fix (createdat)

--- banking-transactions/src/main/java/com/banking/transactions/Transaction.java
+++ banking-transactions/src/main/java/com/banking/transactions/Transaction.java
@@ -1,12 +1,14 @@
 package com.banking.transactions;
 
 import java.math.BigDecimal;
+import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
@@ -14,7 +16,7 @@ import lombok.Data;
 
 @Data
 @Entity
-@Table(name = "transactions")
+@Table(name = "BK_Transaction")
 public class Transaction {
 
 	@Id
@@ -31,5 +33,13 @@ public class Transaction {
 
 	@NotNull
 	@Column(nullable = false)
-	private String sourceAccountId;
+	private String sourceaccountid;
+
+	private Date createdat;
+
+	@PrePersist
+	void createdat() {
+		this.createdat = new Date();
+	}
+
 }
