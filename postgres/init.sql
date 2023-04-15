cf556ef7ced7c1d08c5220aed57e33ea465a02f8
younes-labiod <youneslabiod@gmail.com>
2023-04-15 15:56:56 +0400

    Add updatedAt in custom postgres image

--- postgres/init.sql
+++ postgres/init.sql
@@ -6,7 +6,8 @@ CREATE TABLE IF not exists BK_Transaction (
 	currency varchar(3) not null,
 	amount BIGINT NOT NULL,
 	sourceAccountId varchar(100) not null,
-    createdAt timestamp not null
+    createdAt timestamp not null,
+    updatedAt timestamp not null
 );
 
-INSERT INTO BK_Transaction (currency, amount, sourceAccountId, createdAt ) VALUES ('AED', 20000, 'account123456789', Now());
+INSERT INTO BK_Transaction (currency, amount, sourceAccountId, createdAt, updatedAt ) VALUES ('AED', 20000, 'account123456789', Now(), Now());
